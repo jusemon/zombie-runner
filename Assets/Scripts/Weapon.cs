c968ca830244235db09d0099ec860d7ce53af0c9
Jusemon <juansmm@outlook.com>
2023-04-07 21:18:05 -0500

    Add muzzle play effect

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPSCamera;
     [SerializeField] float range = 100;
     [SerializeField] float damage = 20;
+    [SerializeField] ParticleSystem muzzleFlash;
 
     // Update is called once per frame
     void Update()
@@ -19,6 +20,17 @@ public class Weapon : MonoBehaviour
     }
 
     private void Shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
     {
         if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out RaycastHit hit, range))
         {
